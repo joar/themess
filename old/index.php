a6787d6216c6cf9b9df8c0426bc5556038c08fd0
Joar Wandborg <joar@wandborg.se>
2013-05-10 22:15:53 +0200

    Initial commit

---
+++ old/index.php
@@ -0,0 +1,45 @@
+<html>
+	<head>
+		<title>The Mess Production Team</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script type='text/javascript' src='jquery.js'></script>
+
+	</head>
+	<body>
+<table id="maintable" width="100%">
+   
+  <tr>
+    <td>&nbsp;</td>
+    <td class="mitten" id="middle"><img id="image" src="we.png"></td>
+   <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td class="mitten" id="middle">
+<div id="texten">
+<h2>The Mess is a four piece songwriting and music production team based in Stockholm, Sweden.</h2>
+<blockquote>We met at Musikmakarna, Songwriters Academy in &Ouml;rnsk&ouml;ldsvik and found that the differences in our musical backgrounds were an advantage when it came to music production and songwriting.</blockquote>
+<p><em>The Mess</em>  was recently formed but has already had some success such as a release with <em>Emerentia</em> with the song Wasting water, songs to be released with <em>Salvatore Ganacci</em> and the Taiwanese artist <em>Genie Chou</em>, songs on hold with <em>Taped Rai</em>, <em>Anders Bagge</em>, <em>Maryjet</em>, <em>The Kennel</em> and the Japanese boyband <em>Kat-tun</em>.</p>
+<blockquote>We feel like this is the beginning of something great and that the next step should be Los Angeles. Our minds are set and our goals are solid!</blockquote>
+<p><em>/ The Mess</em></p>
+<p>Contact us at <em>info@themessproduction.com</em></p></div>
+
+<iframe id="player" width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F85635469%3Fsecret_token%3Ds-hH5NT&amp;auto_play=false&amp;show_artwork=false&amp;color=09113f"></iframe>
+
+</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+		<script type="text/javascript">
+$(document).ready(function(){
+    $("#image").fadeIn(1600);
+    $("#player").fadeIn(3000);  
+    $("h2").fadeIn(1500); 
+    $("p").fadeIn(1700);  
+    $("blockquote").fadeIn(1800); 
+
+});
+</script>
+</body>
+</html>
+
